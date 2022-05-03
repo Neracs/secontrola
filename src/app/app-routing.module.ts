271ac3ed8e9256da4851da4662f6531abaa12d7f
Scaner Reyfreld <tz.neracs@gmail.com>
2022-05-02 19:40:30 -0500

    base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent,
+    children: []
+  },
+  { path: '**',
+    component: AboutComponent
+  },
+  {
+    path: 'about',
+    loadChildren: () => AboutComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
